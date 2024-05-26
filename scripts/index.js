9ef279567022ce736640a5d1f728410341315f8f
unknown <shahinafridi012@gmail.com>
2024-05-26 22:19:22 +0600

    my second commit

---
+++ scripts/index.js
@@ -0,0 +1,38 @@
+function play(){
+    const hideTheHome = document.getElementById('home-screen');
+    const hideTheHomeOn = hideTheHome.classList.add('hidden');
+    console.log(hideTheHomeOn);
+    const showThePlay = document.getElementById('play-ground');
+    const showTheKbd = showThePlay.classList.remove('hidden');
+    console.log(showTheKbd);
+    
+    const alPaBetString = 'abcdefghijklmnopqrstwyz';
+    const alPhaBet = alPaBetString.split('');
+    const randomNumber = Math.random() * 25;
+    const index = Math.round(randomNumber);
+    const alphaDed = alPhaBet[index];
+    console.log(index, alphaDed);
+    // return alphaDed;
+    
+    const alphabetOnScreen = document.getElementById('screen-latter')
+    alphabetOnScreen.innerText = alphaDed;
+
+}
+// function getARandomLatter(){
+//     const alPaBetString = 'abcdefghijklmnopqrstwyz';
+//     const alPhaBet = alPaBetString.split('');
+//     const randomNumber = Math.random() * 25;
+//     const index = Math.round(randomNumber);
+//     const alphaDed = alPhaBet[index];
+//     console.log(index, alphaDed);
+//     return alphaDed;
+// }
+// function continueGame(){
+//     const alphaDed = getARandomLatter();
+//     console.log(alphaDed);
+// }
+// function play(){
+//     continueGame();
+// }
+
+
